6c436d408320cd354b060d7d2dbf96146feb9ce2
kms1601 <141806725+kms1601@users.noreply.github.com>
2024-07-25 11:45:08 +0900

    [level 2] Title: 문자열 압축, Time: 12.54 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        String originalS = s;
+        String[] compressedArr = new String[s.length() / 2 + 1];
+        compressedArr[0] = s;
+
+        for (int i = 1; i <= originalS.length() / 2; i++) {
+            s = originalS;
+            StringBuilder compressed = new StringBuilder();
+            while (true) {
+                int repeat = compressing(s, i);
+                compressed.append(repeat == 1 ? "" : repeat).append(s, 0, i);
+                s = s.substring(i * repeat);
+                if (s.length() < i) {
+                    compressed.append(s);
+                    break;
+                }
+            }
+            compressedArr[i] = compressed.toString();
+        }
+        Arrays.sort(compressedArr, Comparator.comparingInt(String::length));
+        return compressedArr[0].length();
+    }
+
+    private int compressing(String s, int cut) {
+        if (s.length() < cut || !s.substring(0, cut).equals(s.substring(cut, Math.min(2 * cut, s.length())))) {
+            return 1;
+        }
+        return 1 + compressing(s.substring(cut), cut);
+    }
+}
