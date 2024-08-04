68613cd7c4d6352a8083bab2cea3fe6a68472acf
kms1601 <141806725+kms1601@users.noreply.github.com>
2024-08-04 19:39:52 +0900

    [Bronze IV] Title: 사장님 도박은 재미로 하셔야 합니다, Time: 84 ms, Memory: 11820 KB -BaekjoonHub

---
+++ 백준/Bronze/23795. 사장님 도박은 재미로 하셔야 합니다/사장님 도박은 재미로 하셔야 합니다.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        while (true) {
+            int money = Integer.parseInt(br.readLine());
+            if (money == -1) {
+                break;
+            }
+            sum += money;
+        }
+        System.out.println(sum);
+    }
+}
