a099082d2f66351e629b4272af5d0a50f7a4231d
kms1601 <141806725+kms1601@users.noreply.github.com>
2024-08-04 19:11:05 +0900

    [Bronze III] Title: 홀수, Time: 72 ms, Memory: 11452 KB -BaekjoonHub

---
+++ 백준/Bronze/2576. 홀수/홀수.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        int min = 100;
+        for (int i = 0; i < 7; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num % 2 == 1) {
+                sum += num;
+                if (num < min) {
+                    min = num;
+                }
+            }
+        }
+        if (sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+}
