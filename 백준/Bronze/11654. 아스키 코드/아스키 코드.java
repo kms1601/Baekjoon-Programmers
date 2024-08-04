f8450c3eb322cb875bc49af1a931f5ad5bbff48c
kms1601 <141806725+kms1601@users.noreply.github.com>
2024-08-04 19:30:20 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 12780 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        char c = s.next().charAt(0);
+        System.out.println((int) c);
+        s.close();
+    }
+}
