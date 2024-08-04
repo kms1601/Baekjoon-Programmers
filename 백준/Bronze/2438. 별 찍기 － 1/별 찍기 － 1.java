af868313efaf8b4c8e4f8716fc5badafbc1f6d71
kms1601 <141806725+kms1601@users.noreply.github.com>
2024-08-04 18:18:22 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+        br.close();
+    }
+}
