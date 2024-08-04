9dfbe1b9e51865caff9f803381647744f2032000
kms1601 <141806725+kms1601@users.noreply.github.com>
2024-08-04 19:27:08 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 12816 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        System.out.println(str + "??!");
+    }
+}
