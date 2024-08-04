c46015fa66b436c2a316ac2e86b01321b4e7f890
kms1601 <141806725+kms1601@users.noreply.github.com>
2024-08-04 19:41:11 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1956 ms, Memory: 115724 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int count = 1;
+
+    static class Node {
+        Set<Integer> next = new TreeSet<>();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        Node[] nodes = new Node[N + 1];
+        for (int n = 1; n <= N; n++) nodes[n] = new Node();
+
+        for (int m = 0; m < M; m++) {
+            st = new StringTokenizer(br.readLine());
+            int p = Integer.parseInt(st.nextToken());
+            int q = Integer.parseInt(st.nextToken());
+            nodes[p].next.add(q);
+            nodes[q].next.add(p);
+        }
+
+        Arrays.stream(dfs(R, nodes, new boolean[N], new int[N])).forEach(System.out::println);
+    }
+
+    private static int[] dfs(int p, Node[] nodes, boolean[] visit, int[] result) {
+        result[p - 1] = count++;
+        visit[p - 1] = true;
+        for (int next : nodes[p].next) {
+            if (visit[next - 1]) continue;
+            dfs(next, nodes, visit, result);
+        }
+        return result;
+    }
+}
