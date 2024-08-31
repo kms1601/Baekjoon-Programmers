05ead1347f86d5f70a1e10d709c2b5aee83c3506
kms1601 <141806725+kms1601@users.noreply.github.com>
2024-08-31 11:31:55 +0900

    [level 4] Title: 도둑질, Time: 27.15 ms, Memory: 98.6 MB -BaekjoonHub

---
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int[] money) {
+        int n = money.length;
+        int[] dp = new int[n];
+        dp[0] = money[0];
+        dp[2] = money[0] + money[2];
+        int max = Math.max(dp[1], dp[2]);
+        for (int i = 3; i < n - 1; i++) {
+            dp[i] = money[i] + Math.max(dp[i - 2], dp[i - 3]);
+            max = Math.max(max, dp[i]);
+        }
+        
+        dp[0] = 0;
+        dp[1] = money[1];
+        dp[2] = money[2];
+        for (int i = 3; i < n; i++) {
+            dp[i] = money[i] + Math.max(dp[i - 2], dp[i - 3]);
+            max = Math.max(max, dp[i]);
+        }
+        
+        return max;
+    }
+}
