d04e7f7a791fc9a4f926e90509b0d91ea755ec85
kms1601 <141806725+kms1601@users.noreply.github.com>
2024-09-03 22:13:18 +0900

    [level 0] Title: 이진수 더하기, Time: 0.21 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
@@ -0,0 +1,6 @@
+class Solution {
+    public String solution(String bin1, String bin2) {
+        System.out.println();
+        return Integer.toString(Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2), 2);
+    }
+}
