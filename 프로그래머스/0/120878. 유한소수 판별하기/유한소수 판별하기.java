1653ae2cfd83939dc4d3b9dabab3f57de92a74c5
kms1601 <141806725+kms1601@users.noreply.github.com>
2024-09-03 22:44:13 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.04 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int a, int b) {
+        b /= gcd(a, b);
+        while (b % 5 == 0) {
+            b /= 5;
+        }
+        
+        while (b % 2 == 0) {
+            b /= 2;
+        }
+        
+        return b == 1 ? 1 : 2;
+    }
+    
+    private int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+}
