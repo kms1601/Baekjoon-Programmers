ec7b00a5a305226953e9ec4bf5a179f68616c021
kms1601 <141806725+kms1601@users.noreply.github.com>
2025-10-30 18:32:23 +0900

    [Bronze II] Title: 스물셋, Time: 204 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/23251. 스물셋/스물셋.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def main():
+    T = int(input())
+    for _ in range(T):
+        print(int(input()) * 23)
+
+
+if __name__ == "__main__":
+    main()
